fc91dbc3de9e6cc1b6f4548d0f481d1dd77c27ae
Jamie Gardiner <jgardiner@lqgroup.org.uk>
2017-11-01 15:34:18 +0000

    completed bowling kata

---
+++ Bowling/src/Bowling.Domain/Game.cs
@@ -0,0 +1,87 @@
+﻿namespace Bowling.Domain
+{
+    public class Game
+    {
+        public Game()
+        {
+            _currentRoll = 0;
+        }
+
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll;
+        private int _score;
+
+        public void Bowled(int pins)
+        {
+            _rolls[_currentRoll] = pins;
+            _currentRoll++;
+        }
+
+        public int Score()
+        {
+            _score = 0;
+            var rollIndex = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    _score += 10;
+                    AddStrikeBonus(rollIndex);
+                    rollIndex = MoveToNextFrame(rollIndex);
+                    continue;
+                }
+
+                if (IsSpare(rollIndex))
+                {
+                    _score += 10;
+                    AddSpareBonus(rollIndex);
+                    rollIndex = MoveToNextFrame(rollIndex);
+                    continue;
+                }
+
+                _score += CalculateFrameScore(rollIndex);
+                rollIndex = MoveToNextFrame(rollIndex);
+            }
+
+            return _score;
+        }
+
+        private int MoveToNextFrame(int rollIndex)
+        {
+            if (IsStrike(rollIndex))
+            {
+                return rollIndex + 1;
+            }
+            else
+            {
+                return rollIndex + 2;
+            }
+        }
+
+        private int CalculateFrameScore(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex + 1];
+        }
+
+        private void AddSpareBonus(int rollIndex)
+        {
+            _score += _rolls[rollIndex + 2];
+        }
+
+        private void AddStrikeBonus(int rollIndex)
+        {
+            _score += _rolls[rollIndex + 1];
+            _score += _rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return _rolls[rollIndex] == 10;
+        }
+    }
+}
---
+++ Bowling/tests/Bowling.Domain.Tests/GameTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+namespace Bowling.Domain.Tests
+{
+    public class GameTests
+    {
+        private Game _classUnderTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new Game();
+        }
+
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        public void Should_Calculate_Single_Roll_Score(int pins)
+        {
+            _classUnderTest.Bowled(pins);
+
+            Assert.That(_classUnderTest.Score(), Is.EqualTo(pins));
+        }
+
+        [TestCase(4, 3)]
+        [TestCase(6, 2)]
+        [TestCase(9, 7)]
+        public void Should_Calculate_Score_From_Multiple_Rolls(int pins, int numberOfRolls)
+        {
+            for (var i = 0; i < numberOfRolls; i++)
+                _classUnderTest.Bowled(pins);
+
+            Assert.That(_classUnderTest.Score(), Is.EqualTo(pins * numberOfRolls));
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(8)]
+        public void Should_Calculate_Spare_Score(int pinsAfterSpare)
+        {
+            _classUnderTest.Bowled(5);
+            _classUnderTest.Bowled(5);
+            _classUnderTest.Bowled(pinsAfterSpare);
+
+            var score = 10 + pinsAfterSpare + pinsAfterSpare;
+            Assert.That(_classUnderTest.Score(), Is.EqualTo(score));
+        }
+
+        [Test]
+        [TestCase(3, 6)]
+        [TestCase(4, 2)]
+        [TestCase(8, 1)]
+        public void Should_Calculate_Strike_Score(int turn1, int turn2)
+        {
+            _classUnderTest.Bowled(10);
+            _classUnderTest.Bowled(turn1);
+            _classUnderTest.Bowled(turn2);
+
+            var score = (10 + turn1 + turn2) + turn1 + turn2;
+            Assert.That(_classUnderTest.Score(), Is.EqualTo(score));
+        }
+
+        [Test]
+        public void Should_Calculate_Perfect_Score_Game()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                _classUnderTest.Bowled(10);
+            }
+
+            Assert.That(_classUnderTest.Score(), Is.EqualTo(300));
+        }
+    }
+}
